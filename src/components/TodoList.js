bb73f2d3b9eba8d9434dbfe68eb5d529b03f314d
felanadiamondra <felana.diamondra.randriamadison@esti.mg>
2022-02-24 10:25:35 +0300

    filter todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,9 @@ import '../styles/TodoList.css';
 
 class TodoList extends React.Component{
    state = {
-       todos : []
+       todos : [],
+       searchValue : "",
+       selectValue : ""
    }
 
    addTask = todo =>{
@@ -29,7 +31,17 @@ class TodoList extends React.Component{
 //            })
 //        })
 //    }
+   handleChange = e =>{
+       this.setState({
+           searchValue : e.target.value
+       })
+   }
 
+   handleSelectChange = e =>{
+    this.setState({
+        selectValue : e.target.value
+    })
+   }
    handleCheckboxChange = id =>{
        this.setState({
            todos : this.state.todos.map(todo =>{
@@ -49,7 +61,54 @@ class TodoList extends React.Component{
        return (
            <div>
                <AddTodo onSubmit={this.addTask}/>
-                <TodoSearch alltodo={this.todos}/>
+                {/* <TodoSearch alltodo={this.todos}/> */}
+                <h2>Search filtering</h2>
+                <input
+                    type="text"
+                    name="search"
+                    value={this.state.searchValue}
+                    onChange={this.handleChange}
+                />
+                  <select onChange={this.handleSelectChange} value={this.state.selectValue}>
+                    <option>Choose</option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+
+                <table>
+                    <tr>
+                        <th>COCHER TOUT</th>
+                        <th>TACHES</th>
+                        <th>PRIORITE</th>
+                    </tr>
+                        {
+                       
+                       
+                            // this.state.todos.map((res) =>(
+                            //     <Todo key={res.id}
+                            //             task={res.task}
+                            //             priority={res.priority}
+                            //             isChecked={res.isChecked}
+                            //             handleCheckboxChange={() => this.handleCheckboxChange(res.id)}/>
+                            // ))
+
+                        
+                                 this.state.todos.filter(res =>
+                                res.task.match(new RegExp(this.state.searchValue , "i"))).filter(
+                                    res => res.priority.match(new RegExp(this.state.selectValue, "i"))
+                                ).
+                                map(
+                                    res =>(
+                                        <Todo key={res.id}
+                                             task={res.task}
+                                              priority={res.priority}
+                                                isChecked={res.isChecked}
+                                                 handleCheckboxChange={() => this.handleCheckboxChange(res.id)}/>
+                                    )
+                                )
+                        }
+                </table>
            </div>
            
        )
